0a0ddd321d4f5c29e93d26f95800c8ea94447773
Andrea Idini <andrea.idini@matfys.lth.se>
2021-01-03 11:16:26 +0100

    publish script and gitignore

---
+++ publish.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e # If a command fails then the deploy stops
+
+echo "\033[0;32m Deplyoing to GitHub \033[0m\n"
+
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+
+hugo
+mv public ..
+
+git checkout web
+rm -rf  *
+
+mv ../public/* .
+
+git add .
+git commit -am "Automatic commit from script"
+git push
